803d67a45dd184bac4ada2192cf6af424dfcb6a3
joongkeun.kim <joongkeun.kim@lge.com>
2021-12-22 23:25:45 +0900

    transfer CURR_IP to other user

--- func_su.sh
+++ func_su.sh
@@ -4,4 +4,4 @@ LOGIN_MODE=SU
 export LOGIN_IP LOGIN_MODE
 #echo ${LOGIN_IP},${CURR_IP},${USER}
 
-exec /bin/bash --rcfile /data001/joongkeun.kim/.profile "$@"
+#exec /bin/bash --rcfile /data001/joongkeun.kim/.profile "$@"
