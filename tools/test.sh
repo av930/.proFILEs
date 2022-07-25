f19a45d3bcd647e166d38b0b81c48c1327a5a79b
joongkeun.kim <joongkeun.kim@lge.com>
2022-07-25 16:53:12 +0900

    add test script

--- tools/test.sh
+++ tools/test.sh
@@ -1,58 +1,6 @@
 #!/bin/bash
-cat -nA $1
-printf "\n============================================next:\n"
+echo ${BASH_SOURCE}
+echo $(dirname "$0")
 
 
-n=0; while read line; do 
-    printf "%2d: %s\n" $((n=n+1)) "$line"
-done < $1
-read -p "========: simple loop"
 
-IFS='';n=0; while read -r line || [[ -n $line ]]; do 
-    printf "%2d: %s\n" $((n=n+1)) "$line"
-done < $1
-read -p "========: IFS, read -r"
-
-
-IFS='';n=0; cat $1 | while read -r line || [[ -n $line ]]; do
-    printf "%2d: %s\n" $((n=n+1)) "$line"
-done
-read -p "========: same above"
-
-
-readarray -t my_array < $1
-IFS='';n=0; for line in "${my_array[@]}"; do
-  printf "%2d: %s\n" $((n=n+1)) "$line"
-done
-read -p "========: readarray"
-
-
-IFS=$'\n'; n=0; for line in $(cat $1); do 
-    printf "%2d: %s\n" $((n=n+1)) "$line"
-done
-read -p "========: read after cat"
-
-
-echo "----------------------------------------------------------"
-
-
-n=0; while read line; do 
-    printf "%2d: %s\n" $((n=n+1)) "$line"
-done < $1
-read -p "========: simple loop"
-
-n=0; while read line; do 
-    printf "%2d: %s\n" $((n=n+1)) "$line"
-done < <(date)
-read -p "========: simple loop"
-
-n=0; while read line; do 
-    printf "%2d: %s\n" $((n=n+1)) "$line"
-done <<< "$(date)"
-read -p "========: simple loop"
-
-
-n=0; while read line; do 
-    printf "%2d: %s\n" $((n=n+1)) "$line"
-done <<< "$1"
-read -p "========: simple loop"
