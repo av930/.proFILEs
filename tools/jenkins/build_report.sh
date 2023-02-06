b110441689411415d8c9a34cc4f5bb62a5c66310
vc.integrator <vc.integrator@lge.com>
2023-02-06 16:49:58 +0900

    script for jenkins

---
+++ tools/jenkins/build_report.sh
@@ -0,0 +1,24 @@
+################ set for post build script ################ 
+## get console log from Jenkins
+APIKEY=11cec21c35fb88f5d9abc591d844e22532
+
+## get build result
+curl -u $USER:$APIKEY --silent ${JOB_URL}${BUILD_ID}/consoleText -o console.log
+#cat console.log |grep ERROR
+#gawk 'NF > 0' console.log  |sed -n '310,331'p
+
+
+## make report template and insert error message
+cat > result.html << EOF
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>New Page</title>
+  </head>
+  <body>
+    <h1>Hello, World!</h1>
+   ${ERROR}
+  </body>
+</html>
+
+EOF
