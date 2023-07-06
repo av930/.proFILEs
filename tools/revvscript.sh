eb3dae9be0b9b34da22494f89a945dd5ee18c765
joongkeun.kim <joongkeun.kim@lge.com>
2023-07-06 17:39:14 +0900

    exception add

--- tools/revvscript.sh
+++ tools/revvscript.sh
@@ -21,20 +21,34 @@ repo variables $REPO__$VARIABLE, ${REPO_I} ${REPO_COUNT}
 ## USER input or define variable
 cmd=$1
 key=$2
-target_branch=${3/\*/}
+target_branch=${3/\*/} #no need * for all file in gerrit, remove
 base_branch=$4
 dflag="${@: -1}"
+
 tempf=$(mktemp)
-
-
-## for degbugging
+clog()   { printf "${GREEN}$1 ${NCOL} ${@:2}"  ;}
 ## DEBUG=[ false | "printf ${RED}%s${NCOL}\n" ]
 DEBUG=false
-if [ ${dflag} = "debug" ]; then DEBUG="printf ${RED}%s${NCOL}\n"; fi
+if [[ ${dflag} =~ "debug" ]]; then DEBUG="printf ${RED}%s${NCOL}\n"; fi
+
+## input param check
+if [ -z "${target_branch}" ] || [[ ${target_branch} =~ "debug" ]];then 
+    clog "[error] you must check parameter"
+    echo "revvscript.sh <cmd> <key> <branch-name> [<base-branch> <debug option>]"
+    echo "revv <cmd> <branch-name> [<base-branch> <debug option>]"
+    exit 1; 
+fi
+
 
 #no need to close by set +x, repo forall is executed in all sub shell
-showRUN(){ [ "${DEBUG}" = "false" ] || set -x; "$@" ; } 
-clog()   { printf "${GREEN}$1 ${NCOL} ${@:2}"  ;}
+showRUN(){ 
+    case ${dflag} in
+    printdebug) echo "$@"; exit 1;  ;; #show 1st command & exit    
+    debug) set -x; "$@"             ;; #show command & run
+        *) "$@"                     ;; #just run
+    esac
+} 
+
 
 ## default variable & functions
 NCOL='\e[0m'; YELLOW='\e[1;33m'; RED='\e[1;31m'; GREEN='\e[1;32m';
@@ -43,25 +57,33 @@ JSON_IDFY=")]}'"
 
 #### main body 
 ## title for git project
-printf "${BAR}\n${YELLOW}%-10.10b${NCOL} %-12.12b | %-84.84b | %s\n" \
-       "[${REPO_I}/${REPO_COUNT}]" "remote:${REPO_REMOTE}" "project:${REPO_PROJECT}" #"branch:${REPO_RREV}" 
+printf "${BAR}\n${YELLOW}%-10.10b${NCOL} remote:%-12.12b | project:%-84.84b %s\n" \
+       "[${REPO_I}/${REPO_COUNT}]" "${REPO_REMOTE}" "${REPO_PROJECT}" #"| branch:${REPO_RREV}" 
        
 
 ## preprocess
+
+remote=( $(repo manifest |grep review | sed -E 's#.*name="([^"]*[^"]*)".*#\1#') )
+review=( $(repo manifest |grep review | sed -E 's#.*review="([^"]*[^"]*)".*#\1#') )
+#for (( i=0; i < ${#remote[@]}; i++ )); do echo ${remote[$i]}~${review[$i]};done
+
 case ${REPO_REMOTE} in
      __found_error) $DEBUG "case1: error"  && exit 1 
     ;; __skip_case) $DEBUG "case2: skip" && exit 0
-    ;;      devops) $DEBUG "case3: preprocess" 
-                    url=http://vgit.lge.com/${REPO_REMOTE}_test
+    ;;           *) $DEBUG "case3: all" 
+                    for (( i=0; i < ${#remote[@]}; i++ )); do 
+                        if [ "${REPO_REMOTE}" = "${remote[$i]}" ]; then url=${review[$i]}; fi
+                    done
 esac;
 
 case $cmd in
-         branch|branchadd|branchdel) :
+         branch|branchadd|branchdel) target_branch=${target_branch:-${REPO_RREV}}
     ;;                   branchlist) target_branch="?m=${target_branch}"
     ;;    branchaddpre|branchaddpre) target_branch="${target_branch}${REPO_RREV}"
     ;;  branchaddpost|branchdelpost) target_branch="${REPO_RREV}${target_branch}"
 esac
-$DEBUG "target_branch: ${target_branch}" 
+$DEBUG "url: ${url} target_branch: ${target_branch}" 
+
 
 run_command="curl -s -u $REPO__$USER:${key} ${url}/a/projects/${REPO_PROJECT//'/'/'%2F'}/branches/${target_branch} -o ${tempf}"
 ## main handler by git branch
@@ -79,7 +101,7 @@ case ${REPO_RREV} in
                   branch|branchlist) 
                     showRUN ${run_command} 
         ;;     branchadd|branchaddpre|branchaddpost) 
-                    showRUN ${run_command} -X PUT -H "Content-Type: application/json" --data "{"revision": "${REPO_RREV}"}" 
+                    showRUN ${run_command} -X PUT -H "Content-Type: application/json" --data "{"revision": "${base_branch:-${REPO_RREV}}"}" 
         ;;     branchdel|branchdelpre|branchdelpost) 
                     showRUN ${run_command} -X DELETE
         ;;  debug|* ) 
@@ -91,7 +113,7 @@ case ${REPO_RREV} in
             elif [ "$(cat ${tempf} | sed -n '2p')" = "{" ]; then cat "${tempf}" | sed "1d" | jq  -cC ".|{ref,revision}" 
             else cat "${tempf}" | sed '1d'| jq  -cC '.[]|{ref,revision}'
             fi
-        elif [ -z "$(cat ${tempf} | head -1)" ]; then clog "warn" "you must check the result by hand !!"
+        elif [ -z "$(cat ${tempf} | head -1)" ]; then clog "warn" "return success, but you must check the result by hand  !"
         else cat "${tempf}"
         fi
 esac;
