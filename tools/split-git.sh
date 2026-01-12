f657966bd98fbeff227a2fc2d1979c0c41727f3e
joongkeun.kim <joongkeun.kim@lge.com>
2026-01-12 20:47:01 +0900

    misc change

--- tools/split-git.sh
+++ tools/split-git.sh
@@ -60,10 +60,10 @@ if [ ! "$CMD" = "down" ] && [ -n "${REMOTE_NAME}" ]; then
 			popd
 		;;verify|*)
 			pushd "$dir"
-			echo "check remote and add"
+			printf "\e[0;33m check remote and add \e[0m\n"
 			git remote get-url $REMOTE_NAME && { git remote rm $REMOTE_NAME; git remote add $REMOTE_NAME ${REMOTE_ADDR}/${dir}; } || git remote add $REMOTE_NAME ${REMOTE_ADDR}/${dir}
-			echo "check remote is working"
-			git ls-remote --exit-code $REMOTE_NAME $REMOTE_BNCH > /dev/null || { echo "[info] remote or remote branch is not existed"; exit 1; }
+			printf "\e[0;33m check remote is working \e[0m\n"
+			git ls-remote --exit-code $REMOTE_NAME $REMOTE_BNCH > /dev/null || echo "[info] remote or remote branch is not existed"
 			echo "[CMD] git push $REMOTE_NAME HEAD:${REMOTE_BNCH} ${PUSH_OPT}"
 			popd
 		esac
